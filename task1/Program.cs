f0785cd52b67e8c599dd3a39a2c4d2f4a9abf4eb
floweroom <karpenko.alena493@gmail.com>
2022-05-10 09:47:47 +1000

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+
+namespace ConsoleApp64
+{
+
+    public class Person
+    {
+        public string name { get; private set; }
+        public int age { get; private set; }
+
+
+        public Person(string Name, int Age)
+        {
+            
+
+            Name = name;
+            Age = age;
+        }
+
+        public  Person Sub()
+        {
+            return new Person(name, age - 1);
+
+        }
+
+
+        class Program
+        {
+
+            public static void Main()
+            {
+                Person person = new Person("Sasha",  24);
+                
+            }
+
+        }
+    }
+
+}
